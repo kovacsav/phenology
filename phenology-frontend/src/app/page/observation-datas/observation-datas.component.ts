e7ae31a8cd286d3cd49211fbdfa2688d89c0251a
kovacsav <mezbogyo@gmail.com>
2022-02-25 13:56:08 +0100

    photo remove

--- phenology-frontend/src/app/page/observation-datas/observation-datas.component.ts
+++ phenology-frontend/src/app/page/observation-datas/observation-datas.component.ts
@@ -28,20 +28,16 @@ import { CameraComponent } from 'src/app/common/camera/camera.component';
   selector: 'app-observation-datas',
   templateUrl: './observation-datas.component.html',
   styleUrls: ['./observation-datas.component.scss'],
-  providers:[CameraComponent]
+  providers: [CameraComponent],
 })
 export class ObservationDatasComponent implements OnInit {
-  //  @Input() user: User = new User;
 
-  selectedPlant: Plant = new Plant();
-  plants$: Observable<Plant[]> = this.plantService.getAll();
-
-  webcamImage: WebcamImage | null = null;
 
   observation: Observation = new Observation();
   observations$: Observable<Observation[]> = this.observationService.getAll();
 
-  // kell nekünk a bejelentkezett user összes adata
+  // USER
+  // set the signed in user's data
   userString: string = localStorage.getItem('currentUser') || '';
 
   allUser$: Observable<User[]> = this.userService.getAll();
@@ -54,26 +50,34 @@ export class ObservationDatasComponent implements OnInit {
     );
 
   currentUser: User = new User();
+  userID: string = '';
+
+// PLANTS
+
+  selectedPlant: Plant = new Plant();
+  plants$: Observable<Plant[]> = this.plantService.getAll();
+  trees: Plant[] = [new Plant()];
+  shrub: Plant[] = [new Plant()];
+  herbaceous: Plant[] = [new Plant()];
+
+  // IMAGES
+
+  webcamImage: WebcamImage | null = null;
 
   title = 'angular-image-file-upload-tutorial';
 
   @ViewChild('UploadFileInput', { static: false })
   uploadFileInput: ElementRef<HTMLInputElement> = {} as ElementRef;
+
   fileUploadForm: FormGroup = new FormGroup({
     title: new FormControl(''),
     description: new FormControl(''),
   });
+
   uploadArray: FormGroup[] = [];
-  // fileInputLabel: any = '';
-  // @Output() pathEvent: EventEmitter<string> = new EventEmitter<string>();
   fileNames: string[] = [];
-
-  userID: string = '';
-
   imgResult: string = '';
-
   resizedImage: File = {} as File;
-
   file: any;
   localUrl: any;
   localCompressedUrl: any;
@@ -84,9 +88,6 @@ export class ObservationDatasComponent implements OnInit {
   sizeOFCompressedImage: number = 0;
 
 
-  // user$: BehaviorSubject<User | null> = this.authService.currentUserSubject$
-
-  // constructor(private datePipe : DatePipe) {
   constructor(
     private observationService: ObservationService,
     private plantService: PlantService,
@@ -110,12 +111,41 @@ export class ObservationDatasComponent implements OnInit {
   ngOnInit(): void {
     this.fileUploadForm = this.formBuilder.group({
       uploadedImage: [''],
-      webcamImage: ''
+      //webcamImage: ''
+    });
+
+    // separate plants
+    this.plantService.getAll().subscribe((plants) => {
+      (this.trees = plants.filter((plant) => plant.category == 'Fák')).sort(
+        (a, b) => (a.name < b.name ? -1 : a.name > b.name ? 1 : 0)
+      );
+      (this.shrub = plants.filter((plant) => plant.category == 'Cserjék')).sort(
+        (a, b) => (a.name < b.name ? -1 : a.name > b.name ? 1 : 0)
+      );
+      (this.herbaceous = plants.filter(
+        (plant) => plant.category == 'Lágyszárúak'
+      )).sort((a, b) => (a.name < b.name ? -1 : a.name > b.name ? 1 : 0));
     });
   }
 
   // file upload and compress
   // https://medium.com/swlh/compress-image-and-send-it-to-an-api-in-angular-bc48e6ed3835
+
+  async setCurrentUser(): Promise<any> {
+    this.currentUser = await lastValueFrom(this.currentUser$);
+    this.observation.user._id = this.currentUser._id;
+    //console.log(this.currentUser);
+  }
+
+  setPlant(plant: Plant): void {
+    this.observation.plant._id = plant._id;
+    this.setCurrentUser();
+    this.selectedPlant = plant;
+  }
+
+  setPhase(phase: string): void {
+    this.observation.phase = phase;
+  }
 
   selectFile(event: any) {
     var fileName: any;
@@ -130,101 +160,6 @@ export class ObservationDatasComponent implements OnInit {
       reader.readAsDataURL(event.target.files[0]);
     }
   }
-
-  // https://www.npmjs.com/package/ngx-image-compress
-
-  compressFile(image: any, fileName: any) {
-    /*
-    https://docs.microsoft.com/en-us/uwp/api/windows.storage.fileproperties.photoorientation?view=winrt-22000
-    Orientation:
-    Normal - 1 - No rotation needed.
-    The photo can be displayed using its current orientation.
-
-    Unspecified - 0 - An orientation flag is not set.
-    */
-
-    var orientation = 1;
-    /*
-    this.sizeOfOriginalImage =
-      this.imageCompress.byteCount(image) / (1024 * 1024);
-    console.warn('Size in bytes is now:', this.sizeOfOriginalImage);
-    */
-    this.imageCompress
-      .compressFile(image, orientation, 90, 100, 800)
-      .then((result) => {
-        this.imgResultAfterCompress = result;
-        this.localCompressedUrl = result;
-        this.compressedFiles.push(result);
-
-        /*
-        this.sizeOFCompressedImage =
-          this.imageCompress.byteCount(result) / (1024 * 1024);
-        console.warn(
-          'Size in bytes after compression:',
-          this.sizeOFCompressedImage
-        );
-        */
-
-        // create file from byte
-        const imageName = fileName;
-
-        // call method that creates a blob from dataUri
-        const imageBlob = this.dataURItoBlob(
-          this.imgResultAfterCompress.split(',')[1]
-        );
-
-        //imageFile created below is the new compressed file which can be send to API in form data
-        const imageFile = new File([imageBlob], imageName, {
-          type: 'image/jpeg',
-        });
-        this.fileUploadForm.get('uploadedImage')?.setValue(imageFile);
-        this.uploadArray.push(this.fileUploadForm.get('uploadedImage')?.value);
-      });
-  }
-
-  dataURItoBlob(dataURI: any) {
-    const byteString = window.atob(dataURI);
-    const arrayBuffer = new ArrayBuffer(byteString.length);
-    const int8Array = new Uint8Array(arrayBuffer);
-    for (let i = 0; i < byteString.length; i++) {
-      int8Array[i] = byteString.charCodeAt(i);
-    }
-    const blob = new Blob([int8Array], { type: 'image/jpeg' });
-    return blob;
-  }
-
-  onFileSelect(event: any) {
-    //this.compressFile();
-    const files = event.target.files;
-    console.log('files:', files);
-    // this.fileInputLabel = files.name;
-    Array.from(files).forEach((file: any, index: number) => {
-      if (this.fileUploadForm.get('uploadedImage')) {
-        this.fileUploadForm.get('uploadedImage')?.setValue(file);
-
-        console.log('file original:', file);
-
-        /*
-        this.imageCompress.compressFile(file, 1, 1, 1, 800, 800)
-          .then(
-            (compressedImage) => {
-              file = compressedImage;
-              console.log("compressedImage:", compressedImage);
-            });
-*/
-        console.log('file compressed:', file);
-
-        console.log('this.fileUploadForm:', this.fileUploadForm.value);
-        this.uploadArray.push(this.fileUploadForm.get('uploadedImage')?.value);
-        this.fileNames[index] = `${Date.now() + index}.jpg`;
-        this.observation.photo?.push(this.fileNames[index]);
-      }
-    });
-  }
-
-  // onFormSubmit(): any {
-
-  // }
 
   save(): any {
     // if (!this.fileUploadForm.get('uploadedImage')?.value) {
@@ -255,80 +190,79 @@ export class ObservationDatasComponent implements OnInit {
     });
 
     console.log(this.observation);
-    console.log("hello");
+    console.log('hello');
 
     this.observationService
       .create(this.observation)
       .subscribe(() => this.router.navigate(['/']));
+      alert("Köszönjük! Megfigyelését rögzítettük.")
   }
 
-  async setCurrentUser(): Promise<any> {
-    this.currentUser = await lastValueFrom(this.currentUser$);
-    this.observation.user._id = this.currentUser._id;
-    //console.log(this.currentUser);
+
+  // https://www.npmjs.com/package/ngx-image-compress
+
+  compressFile(image: any, fileName: any) {
+    /*
+    https://docs.microsoft.com/en-us/uwp/api/windows.storage.fileproperties.photoorientation?view=winrt-22000
+    Orientation:
+    Normal - 1 - No rotation needed.
+    The photo can be displayed using its current orientation.
+
+    Unspecified - 0 - An orientation flag is not set.
+    */
+
+    var orientation = 1;
+
+    this.imageCompress
+      .compressFile(image, orientation, 90, 100, 800)
+      .then((result) => {
+        this.imgResultAfterCompress = result;
+        this.localCompressedUrl = result;
+        this.compressedFiles.push(result);
+
+        // create file from byte
+        const imageName = fileName;
+
+        // call method that creates a blob from dataUri
+        const imageBlob = this.dataURItoBlob(
+          this.imgResultAfterCompress.split(',')[1]
+        );
+
+        //imageFile created below is the new compressed file which can be send to API in form data
+        const imageFile = new File([imageBlob], imageName, {
+          type: 'image/jpeg',
+        });
+        this.fileUploadForm.get('uploadedImage')?.setValue(imageFile);
+        this.uploadArray.push(this.fileUploadForm.get('uploadedImage')?.value);
+      });
   }
 
-  setPlant(plant: Plant): void {
-    this.observation.plant._id = plant._id;
-    this.setCurrentUser();
-    this.selectedPlant = plant;
-  }
-
-  setPhase(phase: string): void {
-    this.observation.phase = phase;
+  dataURItoBlob(dataURI: any) {
+    const byteString = window.atob(dataURI);
+    const arrayBuffer = new ArrayBuffer(byteString.length);
+    const int8Array = new Uint8Array(arrayBuffer);
+    for (let i = 0; i < byteString.length; i++) {
+      int8Array[i] = byteString.charCodeAt(i);
+    }
+    const blob = new Blob([int8Array], { type: 'image/jpeg' });
+    return blob;
   }
 
   setImagePath(fileName: string): void {}
 
   handleImage(webcamImage: WebcamImage) {
+
     this.webcamImage = webcamImage;
     const fileName: string = `${this.observation.date}-${this.observation.plant.name}.jpg`;
 
-    // create file from byte
-    const imageName = fileName;
-    // call method that creates a blob from dataUri
-    const imageBlob = this.dataURItoBlob(
-      this.webcamImage.imageAsDataUrl.split(',')[1]
-    );
+    this.compressFile(webcamImage.imageAsDataUrl, fileName);
 
-    //imageFile created below is the new compressed file which can be send to API in form data
-    const imageFile = new File([imageBlob], imageName, { type: 'image/jpeg' });
-    console.log("imageFile", imageFile);
-
-    this.fileUploadForm.get('webcamImage')?.setValue(imageFile);
-    this.uploadArray.push(this.fileUploadForm.get('webcamImage')?.value);
-    console.log("uploadArray:", this.uploadArray)
-    //const a = document.createElement('a');
-    //a.setAttribute('download', fileName);
-    //a.setAttribute('href', webcamImage.imageAsDataUrl);
-    //a.click();
-    //this.observation.photo?.push(fileName);
   }
 
-  deletePhoto() {
-    this.uploadArray.pop();
-    this.webcamImage = null;
-    //this.cameraComponent.onOffWebCame();
-//https://stackoverflow.com/questions/37587732/how-to-call-another-components-function-in-angular2
+  onRemove(index: any): void {
+    console.log(index);
+    this.uploadArray.splice(index, 1);
+    this.compressedFiles.splice(index, 1);
   }
-
-  // onFileSelected(event: any) {
-
-  //   const file: File = event.target.files[0];
-
-  //   if (file) {
-
-  //     this.fileName = file.name;
-
-  //     const formData = new FormData();
-
-  //     formData.append("thumbnail", file);
-
-  //     const upload$ = this.http.post("/api/thumbnail-upload", formData);
-  //     this.observationService.uploadFile(formData);
-  //     // this.observationService.uploadFile(formData, this.uploadFileInput);
-
-  //     upload$.subscribe();
-  //   }
-  // }
 }
+
